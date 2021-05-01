4334bba6c8537bd0500456f16dd8f268db33ea0f
Jonathan Poncelet <jonathan.poncelet@talk21.com>
2021-05-01 09:53:09 +0000

    Created simple setup script for HP Pavilion laptop

---
+++ hp-pavilion-tx1000/SetUpBaseSystem.sh
@@ -0,0 +1,35 @@
+InstallExtForBoot() {
+	echo "Installing OnBoot extension: $1"
+
+	if ! tce-load -wi $1; then
+		echo "Failed to install OnBoot extension: $1"
+		exit 1
+	fi
+}
+
+echo "This script will set up the base system."
+echo "It is assumed that a minimal TinyCore system"
+echo "exists, with no Xorg server or desktop environment."
+echo "Press Return or Spacebar to continue, or any other"
+echo "key to cancel."
+
+read -rsn1 input
+
+if [ -n "$input" ]; then
+	echo "Exiting"
+	exit 1
+fi
+
+echo "========== OnBoot Extensions =========="
+echo
+
+echo "Category: Display and Window System"
+InstallExtForBoot Xorg-7.7
+InstallExtForBoot graphics-5.10.3-tinycore
+
+echo "Category: Window Manager"
+InstallExtForBoot openbox
+InstallExtForBoot tint2
+
+echo "Category: Terminals"
+InstallExtForBoot aterm
